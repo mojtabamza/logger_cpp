f707d96a83ac293bade1eafb1280a202fd9aa189
mojtabamza <mojtaba.zamani.65@gmail.com>
2022-10-04 01:11:06 +0330

    edit display to get data from buffer instead of input

--- display.cpp
+++ display.cpp
@@ -2,7 +2,9 @@
 
 void Display_t::show()
 {
+	float temp;
 	system("CLS");
-	cout << "Instant Value : " << Input_t::get_instance()->get_instant_value() << endl;
-
+	//cout << "Instant Value : " << Input_t::get_instance()->get_instant_value() << endl;
+	Buffer_t::get_instance()->get_buffer(instant_value, temp);
+	cout << "Instant Value : " << temp << endl;
 }
--- display.hpp
+++ display.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "inc.hpp"
 #include "input.hpp"
+#include "buffer.hpp"
 
 class Display_t {
 public:
