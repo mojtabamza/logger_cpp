7f99b3afc9873a4579028c21bbc3ddc0945e225a
mojtabamza <mojtaba.zamani.65@gmail.com>
2022-10-14 02:16:38 +0330

    in completing the logger task

--- logger.cpp
+++ logger.cpp
@@ -12,12 +12,18 @@ Logger_t* Logger_t::get_instance()
 void Logger_t::task()
 {
     date_time time_strct = time_obj->get_time_struct();
-    int current_time = (time_strct.time_strct_obj.hh * 3600) + (time_strct.time_strct_obj.mm * 60) + time_strct.time_strct_obj.ss;
+    int current_time = (time_strct.time_strct_obj.hh * 60) + (time_strct.time_strct_obj.mm);
     if (class_time == current_time) {
-        this->ready_to_log();
+        //Buffer_updateing
+        
     }
     else {
         this->class_time = current_time;
+        int time_between_start_and_now = current_time - logger_setting_obj.start_time;
+        if (time_between_start_and_now % logger_setting_obj.interval == 0) {
+            this->ready_to_log(time_strct);
+        }
+        
 
     }
 }
@@ -28,11 +34,20 @@ void Logger_t::set_configs() {
 void Logger_t::get_configs(string& configs) {
 }
 
+void Logger_t::make_log() {
+    cout << "make log" << endl;
+}
+
+void Logger_t::ready_to_log(date_time& time_strct) {
+    cout << "ready to log" << endl;
+}
+
 Logger_t::Logger_t() : time_obj() {
-    class_time = 24 * 60 * 60;
+    class_time = 24 * 60;
     logger_setting_obj.interval = 1;
     logger_setting_obj.log_mode = mean;
-
-
+    logger_setting_obj.status = ENABLE;
+    logger_setting_obj.start_time = _00_00;
+    logger_setting_obj.stop_time = _23_59;
 }
 
--- logger.hpp
+++ logger.hpp
@@ -3,15 +3,27 @@
 #include "buffer.hpp"
 #include "time.hpp"
 
+constexpr auto _00_00 = 0;
+constexpr auto _23_59 = (23 * 60) + 59;
+
 enum logger_mode {
 	min = 0,
 	max,
 	mean,
 };
+enum logger_status {
+	DISABLE = 0,
+	ENABLE = 1,
+};
 struct logger_setting {
 	int interval;
 	logger_mode log_mode;
+	int start_time;
+	int stop_time;
+	logger_status status;
+
 };
+
 
 class Logger_t {
 public:
@@ -20,7 +32,7 @@ public:
 	void set_configs();
 	void get_configs(string& configs);
 	void make_log();
-	void ready_to_log();
+	void ready_to_log(date_time& time_strct);
 	
 private:
 	Logger_t();
