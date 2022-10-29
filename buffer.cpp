29e8aa0fa703c8a49ae161003c74cd2ed0977741
mojtabamza <mojtaba.zamani.65@gmail.com>
2022-10-30 00:14:02 +0330

    complete buffer for four variable and solve some bugs

--- buffer.cpp
+++ buffer.cpp
@@ -12,7 +12,10 @@ Buffer_t* Buffer_t::get_instance() {
 Buffer_t::Buffer_t() : communication() {
 	this->value = 0;
 	this->counter = 0;
+	buff_values.buf_value_0 = this->value;
 	buff_values.buf_value_1 = this->value;
+	buff_values.buf_value_2 = this->value;
+	buff_values.buf_value_3 = this->value;
 }
 
 void Buffer_t::set_configs() {
@@ -27,16 +30,30 @@ void Buffer_t::update_buffer() {
 	logger_setting logger_setting_obj = Logger_t::get_instance()->get_configs();
 	switch (logger_setting_obj.log_mode) {
 	case INSTANTANEOUS:
+		buff_values.buf_value_0 = this->value;
 		buff_values.buf_value_1 = this->value;
+		buff_values.buf_value_2 = this->value;
+		buff_values.buf_value_3 = this->value;
 		break;
 	case MIN:
+		buff_values.buf_value_0 = MIN(buff_values.buf_value_0, this->value);
 		buff_values.buf_value_1 = MIN(buff_values.buf_value_1, this->value);
+		buff_values.buf_value_2 = MIN(buff_values.buf_value_2, this->value);
+		buff_values.buf_value_3 = MIN(buff_values.buf_value_3, this->value);
 		break;
 	case MAX:
+		buff_values.buf_value_0 = MAX(buff_values.buf_value_0, this->value);
 		buff_values.buf_value_1 = MAX(buff_values.buf_value_1, this->value);
+		buff_values.buf_value_2 = MAX(buff_values.buf_value_2, this->value);
+		buff_values.buf_value_3 = MAX(buff_values.buf_value_3, this->value);
+		
 		break;
 	case MEAN:
+		buff_values.buf_value_0 += this->value;
 		buff_values.buf_value_1 += this->value;
+		buff_values.buf_value_2 += this->value;
+		buff_values.buf_value_3 += this->value;
+		
 		counter++;
 		break;
 	default:
@@ -56,16 +73,33 @@ void Buffer_t::get_buffer(channel_address channel_addr, double& value) {
 	case MIN_VALUE:
 	case MAX_VALUE:
 		this->get_value(channel_addr, value);
-		//value = this->buff_values.buf_value_1;
 		break;
 	case MEAN_VALUE:
-		value = buff_values.buf_value_1 / counter;
-		counter = 0;
+		switch (channel_addr) {
+		case CHANNEL_0:
+			value = buff_values.buf_value_0 / counter;
+			buff_values.buf_value_0 = 0;
+			break;
+		case CHANNEL_1:
+			value = buff_values.buf_value_1 / counter;
+			buff_values.buf_value_1 = 0;
+			break;
+		case CHANNEL_2:
+			value = buff_values.buf_value_2 / counter;
+			buff_values.buf_value_2 = 0;
+			break;
+		case CHANNEL_3:
+			value = buff_values.buf_value_3 / counter;
+			buff_values.buf_value_3 = 0;
+			break;
+		}
+		if (channel_addr == THE_LAST_CHANNEL) {
+			counter = 0;
+		}
 		break;
 	default:
 		break;
 	}
-	
 }
 
 void Buffer_t::get_instantaneous_value(channel_address channel_addr, double& value) {
@@ -88,13 +122,16 @@ void Buffer_t::get_instantaneous_value(channel_address channel_addr, double& val
 void Buffer_t::get_value(channel_address channel_addr, double& value) {
 	switch (channel_addr) {
 	case CHANNEL_0:
-		value = buff_values.buf_value_1;
+		value = buff_values.buf_value_0;
 		break;
 	case CHANNEL_1:
+		value = buff_values.buf_value_1;
 		break;
 	case CHANNEL_2:
+		value = buff_values.buf_value_2;
 		break;
 	case CHANNEL_3:
+		value = buff_values.buf_value_3;
 		break;
 	default:
 		break;
