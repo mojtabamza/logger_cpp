5f3923c04f0d52ccda084b999dfb9211b4e02eed
mojtabamza <mojtaba.zamani.65@gmail.com>
2022-11-01 01:40:07 +0330

    add counter to MAX and MIN mode - solve some bugs

--- buffer.cpp
+++ buffer.cpp
@@ -24,42 +24,63 @@ void Buffer_t::set_configs() {
 }
 
 void Buffer_t::task() {
-	this->value = Input_t::get_instance()->get_instant_value();
+	//this->value = Input_t::get_instance()->get_instant_value();
 }
 
 void Buffer_t::update_buffer() {
+	this->value = Input_t::get_instance()->get_instant_value();
+
+	vector<double> temp(CHANNEL_COUNTER, 0);
+	for (int i = 0; i < CHANNEL_COUNTER; i++) {
+		temp[i] = Input_t::get_instance()->get_instant_value();
+	}
+
 	logger_setting logger_setting_obj = Logger_t::get_instance()->get_configs();
 	switch (logger_setting_obj.log_mode) {
 	case INSTANTANEOUS:
-		buff_values.buf_value_0 = this->value;
-		buff_values.buf_value_1 = this->value;
-		buff_values.buf_value_2 = this->value;
-		buff_values.buf_value_3 = this->value;
+		buff_values.buf_value_0 = temp[0];
+		buff_values.buf_value_1 = temp[1];
+		buff_values.buf_value_2 = temp[2];
+		buff_values.buf_value_3 = temp[3];
 		break;
 	case MIN:
-		buff_values.buf_value_0 = MIN(buff_values.buf_value_0, this->value);
-		buff_values.buf_value_1 = MIN(buff_values.buf_value_1, this->value);
-		buff_values.buf_value_2 = MIN(buff_values.buf_value_2, this->value);
-		buff_values.buf_value_3 = MIN(buff_values.buf_value_3, this->value);
+		if (counter == 0) {
+			buff_values.buf_value_0 = temp[0];
+			buff_values.buf_value_1 = temp[1];
+			buff_values.buf_value_2 = temp[2];
+			buff_values.buf_value_3 = temp[3];
+		} 
+		else {
+			buff_values.buf_value_0 = MIN(buff_values.buf_value_0, temp[0]);
+			buff_values.buf_value_1 = MIN(buff_values.buf_value_1, temp[1]);
+			buff_values.buf_value_2 = MIN(buff_values.buf_value_2, temp[2]);
+			buff_values.buf_value_3 = MIN(buff_values.buf_value_3, temp[3]);
+		}
 		break;
 	case MAX:
-		buff_values.buf_value_0 = MAX(buff_values.buf_value_0, this->value);
-		buff_values.buf_value_1 = MAX(buff_values.buf_value_1, this->value);
-		buff_values.buf_value_2 = MAX(buff_values.buf_value_2, this->value);
-		buff_values.buf_value_3 = MAX(buff_values.buf_value_3, this->value);
-		
+		if (counter == 0) {
+			buff_values.buf_value_0 = temp[0];
+			buff_values.buf_value_1 = temp[1];
+			buff_values.buf_value_2 = temp[2];
+			buff_values.buf_value_3 = temp[3];
+		}
+		else {
+			buff_values.buf_value_0 = MAX(buff_values.buf_value_0, temp[0]);
+			buff_values.buf_value_1 = MAX(buff_values.buf_value_1, temp[1]);
+			buff_values.buf_value_2 = MAX(buff_values.buf_value_2, temp[2]);
+			buff_values.buf_value_3 = MAX(buff_values.buf_value_3, temp[3]);
+		}
 		break;
 	case MEAN:
-		buff_values.buf_value_0 += this->value;
-		buff_values.buf_value_1 += this->value;
-		buff_values.buf_value_2 += this->value;
-		buff_values.buf_value_3 += this->value;
-		
-		counter++;
+		buff_values.buf_value_0 += temp[0];
+		buff_values.buf_value_1 += temp[1];
+		buff_values.buf_value_2 += temp[2];
+		buff_values.buf_value_3 += temp[3];
 		break;
 	default:
 		break;
 	}
+	counter++;
 }
 
 int Buffer_t::get_buffer_size()
@@ -69,7 +90,7 @@ int Buffer_t::get_buffer_size()
 
 void Buffer_t::get_buffer(channel_address channel_addr, double& value) {
 	logger_setting logger_setting_obj = Logger_t::get_instance()->get_configs();
-	if (counter == 0) {
+	if (counter == 0 && logger_setting_obj.log_mode == MEAN) {
 		value = INVALID_VALUE_ERR;
 		return;
 	}
@@ -78,6 +99,10 @@ void Buffer_t::get_buffer(channel_address channel_addr, double& value) {
 	case MIN_VALUE:
 	case MAX_VALUE:
 		this->get_value(channel_addr, value);
+		if (channel_addr == THE_LAST_CHANNEL) {
+			this->sample_count = counter;
+			counter = 0;
+		}
 		break;
 	case MEAN_VALUE:
 		switch (channel_addr) {
